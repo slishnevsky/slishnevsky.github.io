9fb8945de4ed4a147bf745bb4f17901563fe747a
Slava Lishnevsky <slishnevsky@gmail.com>
2023-06-07 17:42:51 -0400

    bookmarks done

--- bookmarks.js
+++ bookmarks.js
@@ -1,42 +1,36 @@
-// Assuming you have a JSON file named 'data.json'
-// with an array of items in the following format:
-// [
-//   { "id": 1, "name": "Item 1" },
-//   { "id": 2, "name": "Item 2" },
-//   { "id": 3, "name": "Item 3" }
-// ]
-
 // Function to create the HTML list
-function createList(jsonData, container) {
+function createList(container, data, category) {
   // Get the container element where the list will be placed
   // const container = document.getElementById('list-container');
 
   // Create an unordered list element
-  const ul = document.createElement('ul');
-  ul.setAttribute('class', 'list-group');
+  const listGroup = document.createElement('div');
+  listGroup.setAttribute('class', 'list-group');
 
   // Iterate over the JSON data and create list items
-  for (let i = 0; i < jsonData['Магазины'].length; i++) {
-    const item = jsonData['Магазины'][i];
+  for (let i = 0; i < data[category].length; i++) {
+    const item = data[category][i];
 
     // Create a list item element
-    const li = document.createElement('li');
-    li.setAttribute('class', 'list-group-item');
-    li.textContent = item.title;
+    const listItem = document.createElement('a');
+    listItem.setAttribute('class', 'list-group-item');
+    listItem.setAttribute('href', item.flyer);
+    listItem.setAttribute('target', 'blank');
+    listItem.textContent = item.title;
 
     // Append the list item to the unordered list
-    ul.appendChild(li);
+    listGroup.appendChild(listItem);
   }
 
   // Append the unordered list to the container element
-  container.appendChild(ul);
+  container.appendChild(listGroup);
 }
 
-function createBookmarks(container) {
+function createBookmarks(container, category) {
   // Fetch the JSON data from a file
   fetch('bookmarks.json')
     .then(response => response.json())
-    .then(data => createList(data, container))
+    .then(data => createList(container, data, category))
     .catch(error => console.error(error));
 }
 
