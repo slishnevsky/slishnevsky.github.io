af7d678b4e179ec492306ea8219ff1bd8dd38ff9
Slava Lishnevsky <slishnevsky@gmail.com>
2023-06-08 19:42:27 -0400

    clock, calendar, bookmarks and news working

---
+++ datetime.js
@@ -0,0 +1,14 @@
+String.prototype.toTitleCase = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function getDateTime(container) {
+    const date = new Date();
+    const day = date.toLocaleString('ru-RU', { dateStyle: 'full' });
+    const time = date.toLocaleString('en-En', { timeStyle: 'medium', hour12: true });
+
+    container.textContent = day.toTitleCase() + ' ' + time;
+
+    setInterval(() => { getDateTime(container); }, 1000); // refresh time every minute
+}
+
