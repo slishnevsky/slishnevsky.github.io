46ca73bb3091bb890439d166905fb496499b0b0f
Slava Lishnevsky <slishnevsky@gmail.com>
2023-12-06 22:38:03 -0500

    updated datetime panel

--- js/index.js
+++ js/index.js
@@ -7,9 +7,9 @@ function getDateTime(container) {
   const day = date.toLocaleString('ru-RU', { weekday: 'long', day: '2-digit', month: 'long' });
   const time = date.toLocaleString('en-En', { hour12: true, hour: '2-digit', minute: '2-digit' });
 
-  container.textContent = day.toTitleCase() + ' ' + time;
+  container.innerHTML = day.toTitleCase() + ' <a class="btn btn-xs btn-primary" href="https://www.youtube.com/@euronewsru/videos" target="_blank">последние новости</a>';
 
-  setInterval(() => { getDateTime(container); }, 60000); // refresh time every minute
+  // setInterval(() => { getDateTime(container); }, 60000); // refresh time every minute
 }
 
 function getTranslation(container, text) {
