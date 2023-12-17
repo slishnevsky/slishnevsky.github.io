1e50bd058bb2c2416af7ef12bd87319aaccd8ce3
Slava Lishnevsky <slishnevsky@gmail.com>
2023-12-17 16:08:31 -0500

    updated

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -19,23 +19,20 @@ function createBookmarks(container, data, category) {
     // Create a list item element
     const listItem = document.createElement('a');
     listItem.className = 'list-group-item';
-    listItem.href = bookmark.link;
+    listItem.href = (category == 'Флаерсы') ? bookmark.link2 : bookmark.link;
     listItem.target = 'blank';
-    // const splitstring1 = 'Canada Flyers';
-    // const splitstring2 = 'Canada Shopping';
-    // if (bookmark.title.includes(splitstring1)) {
-    //   listItem.innerHTML = '<strong>' + bookmark.title.split(splitstring1)[0] + '</strong>' + splitstring1;
-    // } else if (bookmark.title.includes(splitstring2)) {
-    //   listItem.innerHTML = '<strong>' + bookmark.title.split(splitstring2)[0] + '</strong>' + splitstring2;
-    // } else {
-    //   listItem.textContent = bookmark.title;
-    // }
-
     listItem.innerHTML = bookmark.title;
 
     let img = document.createElement('img');
     img.className = 'icon pull-left';
-    img.src = 'https://s2.googleusercontent.com/s2/favicons?domain=' + bookmark.link;
+    // img.src = 'https://s2.googleusercontent.com/s2/favicons?domain=https://www.circulars.ca/real-canadian-superstore/'; // + bookmark.link;
+    if (category == 'Флаерсы') img.src = 'assets/icon1.png';
+    if (category == 'Магазины') img.src = 'assets/icon2.png';
+    if (category == 'Сервисы') img.src = 'assets/icon3.png';
+      
+    
+    
+
     listItem.appendChild(img);
 
     // Append the list item to the unordered list
