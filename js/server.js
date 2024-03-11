d2544ec3c51a1da98f2bb9db865fef52ab74968a
Slava Lishnevsky <slishnevsky@gmail.com>
2024-03-11 15:37:38 -0400

    added xml-js, keep testing

--- js/server.js
+++ js/server.js
@@ -1,18 +1,19 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+import convert from 'xml-js'
 
-import express from 'express'
-import fetch from 'node-fetch'
-import cors from 'cors'
-
-const app = express()
+const app = express();
 
 app.use(cors())
 
 app.get('/', async (req, res) => {
-  const response = await fetch('https://news.knopka.ca/rss')
-  const data = response.json()
-  res.json(data)
+  const response = await fetch('https://news.knopka.ca/rss');
+  const data = await response.text();
+  const json = convert.xml2json(data);
+  return json;
 })
 
 app.listen(3000, () => {
-  console.log('listening on port 3000')
+  console.log('listening on port 3000');
 })
